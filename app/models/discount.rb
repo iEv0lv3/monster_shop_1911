35067da00d03c5de5bdce0403749c3c0863bf4f9
Jordan <jordan.wallace.williams@gmail.com>
2020-03-03 21:49:18 -0700

    Update discount model validations to be more restrictive

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,7 +1,7 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
 
-  validates_presence_of :name,
-                        :item_count,
-                        :percent
+  validates_presence_of :name, length: { maximum: 25 }
+  validates_presence_of :item_count, numericality: true, greater_than: 0, less_than: 10000
+  validates_presence_of :percent, numericality: true, greater_than: 0, less_than: 100
 end
